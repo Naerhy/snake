23669b2ff7edf5c0ae62760d5180f86bdee40811
Naerhy <naerhy@protonmail.com>
2022-05-19 01:44:47 +0200

    Add linked list logic and initial snake creation

--- inc/snake.h
+++ inc/snake.h
@@ -6,7 +6,7 @@
 #include "libny.h"
 
 #define BOARD_WIDTH (20)
-#define BOARD_HEIGHT (10)
+#define BOARD_HEIGHT (15)
 #define BLOCK_SIZE (32)
 #define WINDOW_WIDTH (BOARD_WIDTH * BLOCK_SIZE)
 #define WINDOW_HEIGHT (BOARD_HEIGHT * BLOCK_SIZE)
--- src/snake.c
+++ src/snake.c
@@ -1,10 +1,59 @@
+// gcc -Iinc src/snake.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 lib/libny.a
+
 #include "snake.h"
+
+void free_bodypart(void* content)
+{
+	t_bodypart* to_free;
+
+	to_free = (t_bodypart*)content;
+	free(content);
+}
+
+t_bodypart* create_bodypart(int x, int y)
+{
+	t_bodypart *new;
+
+	new = calloc(1, sizeof(t_bodypart));
+	if (!new)
+		return (NULL);
+	new->x = x;
+	new->y = y;
+	return (new);
+}
+
+int init_snake(t_ny_list** snake)
+{
+	t_ny_list* new_node;
+	t_bodypart* bodypart;
+
+	*snake = NULL;
+	for (int i = 0; i < 3; i++)
+	{
+		bodypart = create_bodypart(2 - i, 0);
+		if (!bodypart)
+			return (0);
+		new_node = ny_list_new(bodypart);
+		if (!new_node)
+		{
+			free(bodypart);
+			return (0);
+		}
+		ny_list_append(snake, new_node);
+	}
+	return (1);
+}
 
 int main(void)
 {
-	t_ny_list* list;
+	t_ny_list* snake;
 
-	list = NULL;
+	if (!init_snake(&snake))
+	{
+		ny_list_clear(snake, free_bodypart);
+		return (1);
+	}
+	// set snake default direction
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Snake");
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
@@ -12,9 +61,10 @@ int main(void)
 		BeginDrawing();
 
 		ClearBackground(BLACK);
-
+		
 		EndDrawing();
 	}
 	CloseWindow();
+	ny_list_clear(snake, free_bodypart);
 	return (0);
 }
